443b5634f6fa7fd2c450f9d59b4124bd2390eb37
cdk <cdk@cdkdemac-mini.local>
2016-07-08 14:38:59 +0800

    初始化

---
+++ DWWXPay/DWWXPay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DWWXPay
+//
+//  Created by cdk on 16/7/8.
+//  Copyright © 2016年 dwang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DWWXPay/DWWXPay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DWWXPay
+//
+//  Created by cdk on 16/7/8.
+//  Copyright © 2016年 dwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
